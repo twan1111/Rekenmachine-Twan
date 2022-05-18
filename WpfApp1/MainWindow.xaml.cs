1031473e94415cf8390cdc29210189fd83373120
Twan Stelt <twanvdstelt6@gmail.com>
2022-05-18 11:59:18 +0200

    Nieuwe versie, alle knoppen werken

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -32,8 +32,171 @@ namespace WpfApp1
 
         private void btn1_Click_1(object sender, RoutedEventArgs e)
         {
-            number1 = (number1 * 10) + 1;
-            display.Text = number1.ToString();
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 1;
+                display.Text = number1.ToString();
+            } else
+            {
+                number2 = (number2 *10) + 1;
+                display.Text = number2.ToString();
+            }
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 2;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 2;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn3_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 3;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 3;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 4;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 4;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 5;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 5;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 6;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 6;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 7;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 7;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 8;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 8;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 9;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 9;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 0;
+                display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 0;
+                display.Text = number2.ToString();
+            }
+        }
+
+        private void btnplus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            display.Text = "0";
+        }
+
+        private void btnmin_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+            display.Text = "0";
+        }
+
+        private void btnkeer_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "x";
+            display.Text = "0";
+        }
+
+        private void btndelen_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "/";
+            display.Text = "0";
+        }
+
+        private void btnis_Click(object sender, RoutedEventArgs e)
+        {
+            operation case {
+
+            }
         }
     }
 }
